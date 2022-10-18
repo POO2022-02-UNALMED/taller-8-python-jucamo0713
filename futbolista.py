baf41eb2f7adec709992e8493413fc343d477860
jucamo0713 <jucamo.0713@gmail.com>
2022-10-18 05:22:58 -0500

    finish

---
+++ futbolista.py
@@ -0,0 +1,47 @@
+from persona import Persona
+from deportista import Deportista
+
+
+class Futbolista(Persona, Deportista):
+    _listaFutbolistas = []
+
+    def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
+        Persona.__init__(self, nombre, edad, altura, sexo)
+        Deportista.__init__(self, "Futbol", añosPracticando)
+        self._golesMarcados = golesMarcados
+        self._tarjetasRojas = tarjetasRojas
+        self._piernaHabil = piernaHabil
+        self._listaFutbolistas.append(self)
+
+    def getGolesMarcados(self):
+        return self._golesMarcados
+
+    def setGolesMarcados(self, golesMarcados):
+        self._golesMarcados = golesMarcados
+
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+
+    def setTarjetasRojas(self, tarjetasRojas):
+        self._tarjetasRojas = tarjetasRojas
+
+    def getPiernaHabil(self):
+        return self._piernaHabil
+
+    def setPiernaHabil(self, piernaHabil):
+        self._piernaHabil = piernaHabil
+
+    @classmethod
+    def getListaFutbolistas(cls):
+        return cls._listaFutbolistass
+
+    @classmethod
+    def setListaFutbolistas(cls, listaFutbolistas):
+        cls._listaFutbolistas = listaFutbolistas
+
+    def __str__(self):
+        return "Mi nombre es " + Persona.getNombre(self) + \
+               " soy profesional en el deporte " + Deportista.getDeporte(self) + \
+               " Tengo " + str(Persona.getEdad(self)) + \
+               " años de edad y llevo " + str(Deportista.getAñosPracticando(self)) + \
+               " años en el deporte"
